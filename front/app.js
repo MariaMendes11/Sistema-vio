7483b2cc31cf8df754bd13414763f8f221232113
MariaMendes11 <mcmendes900@gmail.com>
2024-10-16 09:03:50 -0300

    16-10

--- front/app.js
+++ front/app.js
@@ -28,5 +28,38 @@ document
             }, 
             //Transforma os dados do formulário em uma string json para serem enviados no corpo da requisição
             body: JSON.stringify({name, cpf, email, password}),
-        })
-    });
+         })
+         .then((response) => {
+            // tratamento da resposta do servidor / api
+            if (response.ok) {
+              // verifica se a resposta foi bem-sucedida (status: 20*)
+              return response.json();
+            } // --- fechamento 'response.ok'
+            // convertendo o erro em formato JSON
+            return response.json().then((err) => {
+              // mensagem retornada do servidor, acessa pela chave 'error'
+              throw new Error(err.error);
+            }); // --- fechamento 'response error'
+          }) // --- fechamento 'response'
+          .then((data) => {
+            // executa a resposta de sucesso  - retorna ao usuario final
+            // exibe alerta com o nome do usuario com o nome que acabou de ser cadastrado (front)
+            // alert("Usuário cadastrado com sucesso: " + data.user.name);
+    
+            alert(data.message);
+    
+            // alert("Usuário Cadastrado: " + data.user);
+    
+            // exibe o log no terminal
+            console.log("Usuário Cadastrado: " + data.user);
+    
+            // limpa os campos do formulario, após o sucesso do cadastro
+            document.getElementById("formulario-registro").reset()
+          }) // --- fechamento 'data'
+          //captura qualquer erro que ocorra durante o processo de requisição/ resposta
+          .catch((error) => {
+            // exibe alerta no (front) com erro processado
+            alert("Erro no cadastro: " + error.message);
+            console.error("Erro:", error.message);
+          }); // --- fechamento 'catch(error)'
+      });
